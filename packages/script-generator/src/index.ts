de5b2c5109309f25af501b0afd1804ddbea189d3
Michal Matuszewski <90936684+MichalMatu@users.noreply.github.com>
2026-09-21 19:28:05 +0200

    Export runtime config update generator

--- packages/script-generator/src/index.ts
+++ packages/script-generator/src/index.ts
@@ -1,5 +1,6 @@
 export * from './shelly/config.js';
 export * from './shelly/runtimeConfig.js';
+export * from './shelly/runtimeConfigUpdate.js';
 export * from './shelly/hash.js';
 export * from './shelly/generate.js';
 export * from './shelly/decode.js';
