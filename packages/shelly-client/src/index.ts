7ae9dc810f78f310e855e8569d7e525abe626f7c
Michal Matuszewski <90936684+MichalMatu@users.noreply.github.com>
2026-09-25 03:07:24 +0200

    feat: export Shelly BLE RPC transport

--- packages/shelly-client/src/index.ts
+++ packages/shelly-client/src/index.ts
@@ -3,6 +3,8 @@ export * from './deviceIdentity.js';
 export * from './rpc/timeout.js';
 export * from './rpc/validators.js';
 export * from './rpc/fetch.js';
+export * from './rpc/bleProtocol.js';
+export * from './rpc/ble.js';
 export * from './scripts/hash.js';
 export * from './scripts/install.js';
 export * from './scripts/read.js';
